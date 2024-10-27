ce1a25b9f671a0e381df84908b18615a2b90519b
softdeveloper23 <brannongarrett77@gmail.com>
2024-10-28 00:57:36 +0100

    Added printGameField method

--- src/main/java/battleship/Main.java
+++ src/main/java/battleship/Main.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // print game field
+        printGameField();
+    }
+    // Print game field.
+    private static void printGameField() {
+        System.out.print("  ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + 1 + " ");
+        }
+        System.out.println();
+        for (char letter = 'A'; letter <= 'J'; letter++) {
+            System.out.print(letter + " ");
+            for (int j = 0; j < 10; j++) {
+                System.out.print("~ ");
+            }
+            System.out.println();
+        }
     }
 }
