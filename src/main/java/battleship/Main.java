8e9db465a6a4c9fa35042021bbb7a537dc2cafe8
softdeveloper23 <brannongarrett77@gmail.com>
2024-10-28 01:11:49 +0100

    Added shipCoordinates method

--- src/main/java/battleship/Main.java
+++ src/main/java/battleship/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         printGameField();
     }
     // Print game field.
@@ -20,5 +21,22 @@ public class Main {
             }
             System.out.println();
         }
+    }
+    // Ship coordinates
+    private static void shipCoordinates(Scanner scanner) {
+        System.out.println("Enter the coordinates of the ship:");
+        String coordinates = scanner.nextLine();
+        // TODO: Add validation check to prevent errors.
+        String[] tokens = coordinates.split(" ");
+        // TODO: Add method to figure out length.
+        // TODO: Add method to figure out parts.
+    }
+
+    private static void shipLength() {
+        // TODO: Add code.
+    }
+
+    private static void shipParts() {
+        // TODO: Add code.
     }
 }
